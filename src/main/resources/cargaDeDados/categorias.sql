9a45ed2a190825f2c8047ec407ff279fa7384449
estaeldev <estael.dev@gmail.com>
2023-03-30 22:13:42 -0300

    Added/arquivos config e estatico para o projeto

---
+++ src/main/resources/cargaDeDados/categorias.sql
@@ -0,0 +1,4 @@
+INSERT INTO public.categorias (id, nome) VALUES(1,'entradas');
+INSERT INTO public.categorias (id, nome) VALUES(2,'saladas');
+INSERT INTO public.categorias (id, nome) VALUES(3,'pratos principais');
+INSERT INTO public.categorias (id, nome) VALUES(4,'sobremesas');
